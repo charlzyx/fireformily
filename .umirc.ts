3dd96f262ebeb1c8fe03f9b59f194225ed0e6651
charlzyx <charlzyx@qq.com>
2022-09-09 17:45:39 +0800

    docs: ✏️ fix github pages public path

--- .umirc.ts
+++ .umirc.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   mode: 'site',
   outputPath: './doc-site',
+  publicPath: process.env.NODE_ENV === 'production' ? '/fireformily/' : '/',
   navs: [
     {
       title: 'GitHub',
